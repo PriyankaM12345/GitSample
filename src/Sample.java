066e226ba4459ec42aa2ef98959adbf80ef63c26
PriyankaM12345 <Priyanka@idsspl.com>
2018-12-13 17:02:19 +0530

    changes

--- src/Sample.java
+++ src/Sample.java
@@ -3,7 +3,7 @@ public class Sample {
 
 	public static void main(String[] arg){
 		System.out.println("Tesing Git...");
-		System.out.println("--------------");
+		System.out.println("111111111111111");
 	}
 	
 }
